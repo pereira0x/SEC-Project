f2f0c3cc1c4da8a3b15ca56857d18777c6d9173a
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2025-03-01 23:18:55 +0000

    feat: add byte array wrapper for comparison

---
+++ depChain/src/main/java/depchain/utils/ByteArrayWrapper.java
@@ -0,0 +1,28 @@
+package depchain.utils;
+
+import java.util.Arrays;
+
+public class ByteArrayWrapper {
+  private final byte[] data;
+  
+  public ByteArrayWrapper(byte[] data) {
+      this.data = data;
+  }
+  
+  @Override
+  public boolean equals(Object other) {
+      if (!(other instanceof ByteArrayWrapper)) {
+          return false;
+      }
+      return Arrays.equals(data, ((ByteArrayWrapper)other).data);
+  }
+  
+  @Override
+  public int hashCode() {
+      return Arrays.hashCode(data);
+  }
+  
+  public byte[] getData() {
+      return data;
+  }
+}
