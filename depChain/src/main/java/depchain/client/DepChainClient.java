b69b4c386a8b6c141ead7f1ca42c5c7593204bab
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-03-20 14:21:35 +0000

    feat: add 6 second timeout for client_reply to arrive

--- depChain/src/main/java/depchain/client/DepChainClient.java
+++ depChain/src/main/java/depchain/client/DepChainClient.java
@@ -16,11 +16,13 @@ public class DepChainClient {
     private ClientLibrary clientLib;
     private int clientPort;
     private int clientId;
+    private final int f;
 
     // constructor
-    public DepChainClient(int clientId, int clientPort) {
+    public DepChainClient(int clientId, int clientPort, int f) {
         this.clientId = clientId;
         this.clientPort = clientPort;
+        this.f = f;
 
         Dotenv dotenv = Dotenv.load();
         String configFilePath = dotenv.get("CONFIG_FILE_PATH");
@@ -50,7 +52,7 @@ public class DepChainClient {
             return;
         }
 
-        this.clientLib = new ClientLibrary(pl, 1, leaderAddr, clientId);
+        this.clientLib = new ClientLibrary(pl, 1, leaderAddr, clientId, f);
     }
 
     public static void main(String[] args) throws Exception {
@@ -62,7 +64,7 @@ public class DepChainClient {
         int clientId = Integer.parseInt(args[0]);
         int clientPort = Integer.parseInt(args[1]);
 
-        DepChainClient client = new DepChainClient(clientId, clientPort);
+        DepChainClient client = new DepChainClient(clientId, clientPort, 1);
 
         final Scanner scanner = new Scanner(System.in);
 
