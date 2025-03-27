62797bfe5e82f23829463b5a4ecd71381cee7aca
pereira0x <jose.a.pereira03@gmail.com>
2025-03-27 11:07:20 +0000

    feat: merge ISTCoin and ACL into one contract

--- remix/ISTCoin.sol
+++ remix/ISTCoin.sol
@@ -2,25 +2,46 @@
 pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./AccessControl.sol"; // Import the AccessControl contract
 
 contract ISTCoin is ERC20 {
-    AccessControl private accessControl;
+    address public owner;
+    mapping(address => bool) private blacklist;
 
-    constructor(address accessControlAddress) ERC20("IST Coin", "IST") {
-        accessControl = AccessControl(accessControlAddress);
+    constructor() ERC20("IST Coin", "IST") {
+        owner = msg.sender;
         _mint(msg.sender, 100000000 * 10 ** decimals()); // Mint 100 million tokens
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    // Access Control Functions
+    function addToBlacklist(address _address) public onlyOwner returns (bool) {
+        blacklist[_address] = true;
+        return true;
+    }
+
+    function removeFromBlacklist(address _address) public onlyOwner returns (bool) {
+        blacklist[_address] = false;
+        return true;
+    }
+
+    function isBlacklisted(address _address) public view returns (bool) {
+        return blacklist[_address];
+    }
+
+    // Overridden ERC20 Functions with Access Control
     function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(!accessControl.isBlacklisted(msg.sender), "Sender is blacklisted");
-        require(!accessControl.isBlacklisted(recipient), "Recipient is blacklisted");
+        require(!isBlacklisted(msg.sender), "Sender is blacklisted");
+        require(!isBlacklisted(recipient), "Recipient is blacklisted");
         return super.transfer(recipient, amount);
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-        require(!accessControl.isBlacklisted(sender), "Sender is blacklisted");
-        require(!accessControl.isBlacklisted(recipient), "Recipient is blacklisted");
+        require(!isBlacklisted(sender), "Sender is blacklisted");
+        require(!isBlacklisted(recipient), "Recipient is blacklisted");
         return super.transferFrom(sender, recipient, amount);
     }
 }
